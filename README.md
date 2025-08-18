bfb9b4275fef994ec66491930b5342479398c2d4
Steve Grehan <stevegrehan@Steves-MacBook-Pro.local>
2025-08-19 06:02:48 +1000

    feat: timer

---
+++ README.md
@@ -0,0 +1,14 @@
+# Pomodoro
+
+## Run
+
+```
+npm start
+```
+
+## Build
+
+```
+npm install electron-builder --save-dev
+npm run dist
+```
---
+++ main.js
@@ -0,0 +1,80 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 600,
+    minHeight: 400,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    icon: path.join(__dirname, 'icon.png'), // Optional: add an icon
+    titleBarStyle: 'hiddenInset', // macOS style title bar
+    backgroundColor: '#0a0a0a'
+  });
+
+  mainWindow.loadFile('index.html');
+  
+  // Start in fullscreen if desired
+  // mainWindow.setFullScreen(true);
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+// Create app menu
+function createMenu() {
+  const template = [
+    {
+      label: 'Pomodoro Timer',
+      submenu: [
+        {
+          label: 'About Pomodoro Timer',
+          role: 'about'
+        },
+        { type: 'separator' },
+        {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          click: () => app.quit()
+        }
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Toggle Fullscreen',
+          accelerator: 'Ctrl+Command+F',
+          click: () => {
+            mainWindow.setFullScreen(!mainWindow.isFullScreen());
+          }
+        },
+        { role: 'reload' },
+        { role: 'toggledevtools' }
+      ]
+    }
+  ];
+  
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  createMenu();
+  
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
